bdf4b6eb0c0edb75eea333345b216d09b66bc86d
Basak Ulcay <basakulcay@BasakAir.local>
2021-02-27 09:41:43 +0300

    questions

---
+++ myscript.js
@@ -0,0 +1,44 @@
+function first() {
+    var x = document.getElementById("first");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function second() {
+    var x = document.getElementById("second");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function third() {
+    var x = document.getElementById("third");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function fourth() {
+    var x = document.getElementById("fourth");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function fifth() {
+    var x = document.getElementById("fifth");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }

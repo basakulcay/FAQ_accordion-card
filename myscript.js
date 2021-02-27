b63f9896aa69b647a4872b79756a3e7601c3d3e7
Basak Ulcay <basakulcay@BasakAir.local>
2021-02-27 13:40:36 +0300

    lines

--- myscript.js
+++ myscript.js
@@ -2,6 +2,8 @@ function first() {
     var x = document.getElementById("first");
     if (x.style.display === "none") {
       x.style.display = "block";
+      document.getElementById('f').style.color='black';
+      document.getElementById('f').style.fontWeight='700';
       document.getElementById('second').style.display='none';
       document.getElementById('third').style.display='none';
       document.getElementById('fourth').style.display='none';
@@ -15,6 +17,8 @@ function first() {
     var x = document.getElementById("second");
     if (x.style.display === "none") {
       x.style.display = "block";
+      document.getElementById('s').style.color='black';
+      document.getElementById('s').style.fontWeight='700';
       document.getElementById('first').style.display='none';
       document.getElementById('third').style.display='none';
       document.getElementById('fourth').style.display='none';
@@ -28,6 +32,8 @@ function first() {
     var x = document.getElementById("third");
     if (x.style.display === "none") {
       x.style.display = "block";
+      document.getElementById('t').style.color='black';
+      document.getElementById('t').style.fontWeight='700';
       document.getElementById('first').style.display='none';
       document.getElementById('second').style.display='none';
       document.getElementById('fourth').style.display='none';
@@ -41,7 +47,8 @@ function first() {
     var x = document.getElementById("fourth");
     if (x.style.display === "none") {
       x.style.display = "block";
-      
+      document.getElementById('fo').style.color='black';
+      document.getElementById('fo').style.fontWeight='700';
       document.getElementById('second').style.display='none';
       document.getElementById('third').style.display='none';
       document.getElementById('first').style.display='none';
@@ -55,6 +62,8 @@ function first() {
     var x = document.getElementById("fifth");
     if (x.style.display === "none") {
       x.style.display = "block";
+      document.getElementById('ff').style.color='black';
+      document.getElementById('ff').style.fontWeight='700';
       document.getElementById('second').style.display='none';
       document.getElementById('third').style.display='none';
       document.getElementById('fourth').style.display='none';
